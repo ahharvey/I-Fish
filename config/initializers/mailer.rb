eea0fc71ac6889596e860714b9a08b381c135f41
febbry <febbry@febbry-M720SRS.(none)>
2012-11-21 13:20:32 +0700

    modified mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port: "587",
   domain: "gmail.com",
   authentication: :plain,
-  user_name: "fajarrizki7@gmail.com",
-  password: 'fajarrizki7',
+  user_name: "noreplymessage124@gmail.com",
+  password: 'importexport123',
   enable_starttls_auto: true
 }
