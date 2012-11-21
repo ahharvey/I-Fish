f91c326a1c44c7dd23d2ae2d2fdb365943aab6ab
febbry <febbry@febbry-M720SRS.(none)>
2012-11-21 14:37:58 +0700

    update import excel data

--- lib/import_excel_data.rb
+++ lib/import_excel_data.rb
@@ -5,43 +5,52 @@ class ImportExcelData
     sheets = xls.sheets
 
     # form A
-    xls.default_sheet = xls.sheets[2]
+    unless xls.sheets['Form A - Fleet'].blank?
+      xls.default_sheet = xls.sheets['Form A - Fleet']
     
     
-    for i in 2..xls.last_row
-      reg = xls.cell(i,"B")
-      name = xls.cell(i,"C")
-      type = xls.cell(i,"D")
-      engine = xls.cell(i,"E")
-      power = xls.cell(i,"F")
-      fishing_area = xls.cell(i,"G")
-      dep_time = xls.cell(i,"H")
-      arr_time = xls.cell(i,"I")
-      sail = xls.cell(i,"J")
-      fuel = xls.cell(i,"K")
-      crew = xls.cell(i,"L")
-      gear = xls.cell(i,"M")
-      weight = xls.cell(i,"N")
-      qty = xls.cell(i,"O")
-      value = xls.cell(i,"P")
-      gear_id = Gear.where("name = ?", gear).first.id rescue nil
+      for i in 2..xls.last_row
+        reg = xls.cell(i,"B")
+        name = xls.cell(i,"C")
+        type = xls.cell(i,"D")
+        engine = xls.cell(i,"E")
+        power = xls.cell(i,"F")
+        fishing_area = xls.cell(i,"G")
+        dep_time = xls.cell(i,"H")
+        arr_time = xls.cell(i,"I")
+        sail = xls.cell(i,"J")
+        fuel = xls.cell(i,"K")
+        crew = xls.cell(i,"L")
+        gear = xls.cell(i,"M")
+        weight = xls.cell(i,"N")
+        qty = xls.cell(i,"O")
+        value = xls.cell(i,"P")
       
-      Landing.create(vessel_ref: reg, vessel_name: name, engine: engine, sail: sail,
-        fuel: fuel, crew: crew, weight: weight, quantity: qty, value: value, time_in: arr_time,
-        time_out: dep_time, gear: gear_id)
+        unless reg.blank?
+          gear_id = Gear.where("name = ?", gear).first.id rescue nil
+      
+          Landing.create(vessel_ref: reg, vessel_name: name, engine: engine, sail: sail,
+            fuel: fuel, crew: crew, weight: weight, quantity: qty, value: value, time_in: arr_time,
+            time_out: dep_time, gear: gear_id)
+        end
+      end
     end
     
     # form B
-    xls.default_sheet = xls.sheets[3]
+    unless xls.sheets['Form B - Catch'].blank?
+      xls.default_sheet = xls.sheets['Form B - Catch']
     
     
-    for i in 2..xls.last_row
-      species = xls.cell(i,"B")
-      length = xls.cell(i,"C")
-      weight = xls.cell(i,"D")
-      fish_id = Fish.where("species = ?", species).first.id rescue nil
+      for i in 2..xls.last_row
+        species = xls.cell(i,"B")
+        length = xls.cell(i,"C")
+        weight = xls.cell(i,"D")
+        unless species.blank?
+          fish_id = Fish.where("species = ?", species).first.id rescue nil
         
-      Catch.create(fish_id: fish_id, length: length, weight: weight)
+          Catch.create(fish_id: fish_id, length: length, weight: weight)
+        end
+      end
     end
     
   end
