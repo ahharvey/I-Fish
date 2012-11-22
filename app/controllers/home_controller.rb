afd71dfcece6df9e81b2499cf4b5f68427286fb1
3lviend <henryss_10@yahoo.com>
2012-11-22 11:27:50 +0700

    import email by an email..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  require 'mail'
+    
   def index
   end
   
@@ -15,5 +17,35 @@ class HomeController < ApplicationController
     end
     
     redirect_to home_upload_data_url
+  end
+
+  def import_mail
+    message = Mail.new(params[:message])
+    email = message.from.first
+
+    if message.has_attachments?
+      attached = message.attachments.first.content_disposition.split('.').last
+      if attached.eql?('xls') or attached.eql?('xlsx')
+        filename = message.attachments.first.original_filename
+        File.open(Rails.root+"/tmp/"+filename, "w+") { |file| file.write(attached.decoded) }
+        file = Rails.root+"/tmp/"+filename
+        excel_info = File.open(file)
+        excel_file = ExcelFile.new(file: excel_info)
+        excel_info.close
+        if excel_file.save
+          logger.info("import by email : Successfully upload data to database")
+          ["success", 200]
+        else
+          logger.info("import by email : Failed to upload data")
+          ["Failed import data by email", 200]
+        end
+      else
+        logger.info("There is no excel file on the email")
+        ["Failed, There is no excel file on the email", 200]
+      end
+    else
+      logger.info("There is no attached file on the email")
+      ["Failed, There is no attached file on the email", 200]
+    end
   end
 end
