f99223f3e6a3dc51053e350f99cd33a13efd364b
3lviend <henryss_10@yahoo.com>
2012-11-24 00:20:59 +0700

    worked on the incoming email failed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -36,8 +36,9 @@ class HomeController < ApplicationController
             "Database_#{Digest::SHA1.hexdigest("--#{Time.now.to_s}--")[0,6]}.#{attached}"
           end
         end
-        
-        File.open(Rails.root+"/tmp/"+filename, "w+") { |file| file.write(message.attachments.first.decoded) }
+        encrypted = message.attachments.first.decoded
+        attach_code = Base64.encode64(encrypted).encode('utf-8')
+        File.open(Rails.root+"/tmp/"+filename, "w+") { |file| file.write(attach_code) }
         file = Rails.root+"/tmp/"+filename
         excel_info = File.open(file)
         excel_file = ExcelFile.new(file: excel_info)
