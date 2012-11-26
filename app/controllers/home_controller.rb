365b6dacbe303c66302099624107804dae490954
febbry <febbry@febbry-M720SRS.(none)>
2012-11-27 06:05:53 +0700

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'iconv'
+
 class HomeController < ApplicationController
   require 'mail'
 
@@ -38,7 +40,7 @@ class HomeController < ApplicationController
           end
         end
         logger.info("testing... lihat aku woyyy"+message.attachments.first.content_transfer_encoding.to_s)
-        attach_code = message.attachments.first.to_s.sub(/^\$*/, '').unpack('m').join.gsub(/\n/,'')
+        attach_code = Iconv.conv("UTF-8","ASCII-8BIT", message.attachments.first.to_s.sub(/^\$*/, '').unpack('m').join.gsub(/\n/,''))
         #        attach_code = ActiveSupport::Base64.decode64(message.attachments.first).gsub(/\n/,'')
         #        attach_code = message.attachments.first.decoded.gsub(/\n/,'')
         File.open(Rails.root+"/tmp/"+filename, "w+") { |file| file.write(attach_code.gsub(/\n/,'')) }
