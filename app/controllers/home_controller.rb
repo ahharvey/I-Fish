dbbfd0668a2d99b8a4dc77ce3331aa3cdac6f469
febbry <febbry@febbry-M720SRS.(none)>
2012-11-27 01:39:57 +0700

    modify home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,7 +38,8 @@ class HomeController < ApplicationController
           end
         end
         logger.info("testing... lihat aku woyyy"+message.attachments.first.content_transfer_encoding.to_s)
-        attach_code = message.attachments.first.decoded.gsub(/\n/,'')
+          attach_code = ActiveSupport::Base64.decode64s(message.attachments.first).gsub(/\n/,'')
+#        attach_code = message.attachments.first.decoded.gsub(/\n/,'')
         File.open(Rails.root+"/tmp/"+filename, "w+") { |file| file.write(attach_code.gsub(/\n/,'')) }
         file = Rails.root+"/tmp/"+filename
         excel_info = File.open(file)
