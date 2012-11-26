07cd6b18f0268ac6501d7451a9ef9b8627470c55
febbry <febbry@febbry-M720SRS.(none)>
2012-11-26 23:32:57 +0700

    modified importing excel data

--- lib/import_excel_data.rb
+++ lib/import_excel_data.rb
@@ -22,13 +22,13 @@ class ImportExcelData
         sail = xls.cell(i,"J")
         fuel = xls.cell(i,"K")
         crew = xls.cell(i,"L")
-        gear = xls.cell(i,"M")
+        gear = xls.cell(i,"M").downcase
         weight = xls.cell(i,"N")
         qty = xls.cell(i,"O")
         value = xls.cell(i,"P")
       
         unless reg.blank?
-          gear_id = Gear.where("name = ?", gear).first.id rescue nil
+          gear_id = Gear.where("LOWER(code) = ?", gear).first.id rescue nil
       
           Landing.create(power: power, fishing_area: fishing_area, type: type, vessel_ref: reg, vessel_name: name, engine: engine, sail: sail,
             fuel: fuel, crew: crew, weight: weight, quantity: qty, value: value, time_in: arr_time,
@@ -43,11 +43,11 @@ class ImportExcelData
     
     
       for i in 2..xls.last_row
-        species = xls.cell(i,"B")
+        species = xls.cell(i,"B").downcase
         length = xls.cell(i,"C")
         weight = xls.cell(i,"D")
         unless species.blank?
-          fish_id = Fish.where("species = ?", species).first.id rescue nil
+          fish_id = Fish.where("LOWER(code) = ?", species).first.id rescue nil
         
           Catch.create(fish_id: fish_id, length: length, weight: weight)
         end
@@ -58,9 +58,9 @@ class ImportExcelData
     if xls.sheets.include?('Survey')
       xls.default_sheet = 'Survey'
     
-      fishery = xls.cell(1,"B")
+      fishery = xls.cell(1,"B").downcase
       kabupaten = xls.cell(2,"B")
-      desa = xls.cell(3,"B")
+      code_desa = xls.cell(3,"B").downcase
       date = xls.cell(4,"B")
       start_time = xls.cell(5,"B")
       end_time = xls.cell(6,"B")
@@ -69,8 +69,8 @@ class ImportExcelData
       catch_measure = xls.cell(9,"B")
         
       unless fishery.blank?
-        desa_id = Desa.where("name = ? AND kabupaten = ?", desa, kabupaten).first.id rescue nil
-        fishery_id = Fishery.where("name = ?", fishery).first.id rescue nil
+        desa_id = Desa.where("LOWER(code) = ?", code_desa).first.id rescue nil
+        fishery_id = Fishery.where("LOWER(code) = ?", fishery).first.id rescue nil
 
         Survey.create(fishery: fishery, fishery_id: fishery_id, desa_id: desa_id, date: date, 
           start_time: start_time, end_time: end_time, observer: fleet_observer,
