7ec6b3a9aea9ddb61127d4ba39caf2625c6c4c3b
3lviend <henryss_10@yahoo.com>
2012-12-03 00:56:27 +0700

    work on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 Desa.destroy_all
-Desa.create(name: "test1 desa", name: "desa1", code: 'desa1')
+Desa.create(name: "desa1", code: 'desa1')
 
 Fish.destroy_all
-Fish.create(family: "Squatinidae", genus: "Squatina", species: "australis", local_name: "Australian Angelshark", english_name: "Australian Angelshark", code: "111")
-Fish.create(family: "Muraenidae", genus: "Gymnothorax", species: "eurostus", local_name: "Abbott's Moray", english_name: "Abbott's Moray", code: "112")
-Fish.create(family: "Ceratodontidae", genus: "Neoceratodus", species: "forsteri", local_name: "Australian Lungfish", english_name: "Australian Lungfish", code: "113")
-Fish.create(family: "Centropomidae", genus: "Lates", species: "calcarifer", local_name: "Barramundi", english_name: "Barramundi", code: "114")
-Fish.create(family: "Serranidae", genus: "Chromileptes", species: "altivelis", local_name: "Barramundi Cod", english_name: "Barramundi Cod", code: "115")
+Fish.create(order: "SCOMBROIDEI", family: "Scombridae", scientific_name: "Auxis rochei", english_name: "Bullet tuna", indonesia_name: "Lisong", code: "BLT")
+Fish.create(order: "SCOMBROIDEI", family: "Scombridae", scientific_name: "Auxis thazard", english_name: "Firgate tuna", indonesia_name: "Tongkol krai", code: "FRI")
+Fish.create(order: "SCOMBROIDEI", family: "Scombridae", scientific_name: "Euthynnus affinis", fishbase_name: "Kawakawa", english_name: "Kawa kawa/Eastern little tuna", indonesia_name: "Tongkol komo", code: "KAW")
+Fish.create(scientific_name: "Katsuwanus pelamis", english_name: "Skipjack tuna", indonesia_name: "Cakalang")
+
+Gear.destroy_all
+Gear.create(alpha_code: "PS1-K", cat_eng: "Surrounding nets", cat_ind: "Jaring lingkar", fao_code: "PS", name: "Pukat cincin pelagis kecil dengan satu kapal", num_code: "01.1.1.1", sub_cat_eng: "With purse lines/Purse seine", sub_cat_ind: "Jaring lingkar bertali kerut", type_ind: "Pukat cincin dengan satu kapal", type_eng: "One boat operated purse seines")
+Gear.create(alpha_code: "PS1-B", cat_eng: "Surrounding nets", cat_ind: "Jaring lingkar", fao_code: "PS", name: "Pukat cincin pelagis besar dengan satu kapal", num_code: "01.1.1.2", sub_cat_eng: "With purse lines/Purse seine", sub_cat_ind: "Jaring lingkar bertali kerut", type_ind: "Pukat cincin dengan satu kapal", type_eng: "One boat operated purse seines")
+Gear.create(alpha_code: "PS2-K", cat_eng: "Surrounding nets", cat_ind: "Jaring lingkar", fao_code: "PS", name: "Pukat cincin group pelagis kecil", num_code: "01.1.2.1", sub_cat_eng: "With purse lines/Purse seine", sub_cat_ind: "Jaring lingkar bertali kerut", type_ind: "Pukat cincin dengan satu kapal", type_eng: "Two boat operated purse seines")
+Gear.create(alpha_code: "PS2-B", cat_eng: "Surrounding nets", cat_ind: "Jaring lingkar", fao_code: "PS", name: "Pukat cincin group pelagis besar", num_code: "01.1.2.2", sub_cat_eng: "With purse lines/Purse seine", sub_cat_ind: "Jaring lingkar bertali kerut", type_ind: "Pukat cincin dengan satu kapal", type_eng: "Two boat operated purse seines")
+Gear.create(alpha_code: "LA", cat_eng: "Surrounding nets", cat_ind: "Jaring lingkar", fao_code: "LA", num_code: "01.2.0", sub_cat_eng: "Without purse lines/lampara", sub_cat_ind: "Jaring lingkar tanpa tali kerut")
