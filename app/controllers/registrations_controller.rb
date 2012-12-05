b21be1c2828aafa3ebee19adb276e9ff248eb74c
Hannes Fostie <hannes@openminds.be>
2012-12-05 23:14:12 +0100

    Changes update registration (devise) controller to allow blank passwords and avatars

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,32 @@
 class RegistrationsController < Devise::RegistrationsController
+  def update
+    # required for settings form to submit when password is left blank
+    if params[:user][:password].blank?
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+
+    @user = User.find(current_user.id)
+    if @user.update_attributes(params[:user])
+      if params[:user][:avatar]
+        render :crop
+      else
+        set_flash_message :notice, :updated
+        # Sign in the user bypassing validation in case his password changed
+        sign_in @user, :bypass => true
+        redirect_to after_update_path_for(@user)
+      end
+    else
+      render "edit"
+    end
+  end
+
   protected
 
   def after_sign_up_path_for(resource)
     new_user_session_path
   end
-  
+
   def after_inactive_sign_up_path_for(resource)
     new_user_session_path
   end
