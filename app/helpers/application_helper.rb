07c6db8dff039c5b9f8761d6db8c3687eebda1e5
ahharvey <ah.harvey@gmail.com>
2012-12-08 22:34:22 +0700

    moved Twitter bootstrap to gem. Restyled index views. Added title and icon helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,12 @@ module ApplicationHelper
   def is_active?(page_name)
     "active" if current_page?(page_name)
   end
+  
+  def title(page_title, show_title = true)
+    page_title = page_title.join(' ') if page_title.respond_to? :join
+    content_for(:title) { h(page_title.to_s) }
+    @show_title = show_title
+  end
 
   def twitterized_type(type)
     case type
@@ -16,6 +22,10 @@ module ApplicationHelper
     else
       type.to_s
     end
+  end
+  
+  def upload_icon
+    content_tag(:i, "", :class=>'icon-upload icon-white')
   end
   
 end
