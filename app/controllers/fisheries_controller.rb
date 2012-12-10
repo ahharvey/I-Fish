7cdea570a3f12b9eb38a14e0fa528b480661ebd2
Richard Sylvester <richard@richard-Lenovo-G570.(none)>
2012-12-10 12:34:33 +0100

    Added date range to fisheries length frequency chart.

--- app/controllers/fisheries_controller.rb
+++ app/controllers/fisheries_controller.rb
@@ -21,26 +21,28 @@ class FisheriesController < InheritedResources::Base
   	col_headers = []
 	month_counts = []
 	from = DateTime.new(Integer(params[:year_from]), Integer(params[:month_from]), 1)
-    to = DateTime.new(Integer(params[:year_to]), Integer(params[:month_to]), 1)
-    (from.year..to.year).each do |y|
-      start_month = (from.year == y) ? from.month : 1
-      end_month = (to.year == y) ? to.month : 12
+  to = DateTime.new(Integer(params[:year_to]), Integer(params[:month_to]), 1)
+  (from.year..to.year).each do |y|
+    start_month = (from.year == y) ? from.month : 1
+    end_month = (to.year == y) ? to.month : 12
 
-      (start_month..end_month).each do |m|
-        col_headers.push "#{Date::MONTHNAMES[m].slice(0,3)} #{y.to_s.slice(2,2)}"
-        landings = []
-        surveys = @fisheries.surveys.select{ |s| s.date_published.year == y && s.date_published.month == m}.each do |s|
-          landings.concat(s.landings)
-        end
-        month_counts.push landings.map{ |l| l.weight / ((l.time_in.to_time - l.time_out.to_time) / 1.hour)}.inject(0, :+)
+    (start_month..end_month).each do |m|
+      col_headers.push "#{Date::MONTHNAMES[m].slice(0,3)} #{y.to_s.slice(2,2)}"
+      landings = []
+      surveys = @fisheries.surveys.select{ |s| s.date_published.year == y && s.date_published.month == m}.each do |s|
+        landings.concat(s.landings)
       end
+      month_counts.push landings.map{ |l| l.weight / ((l.time_in.to_time - l.time_out.to_time) / 1.hour)}.inject(0, :+)
     end
-	  	render json: {:col_headers => col_headers, :month_counts => month_counts }
+  end
+		render json: {:col_headers => col_headers, :month_counts => month_counts }
   end
 
   def graph_length_frequency
   	catches = []
-  	@fisheries = Fishery.find(params[:id]).surveys.each do |s|
+    from = DateTime.new(Integer(params[:year_from]), Integer(params[:month_from]), 1)
+    to = DateTime.new(Integer(params[:year_to]), Integer(params[:month_to]), 1)
+  	@fisheries = Fishery.find(params[:id]).surveys.where(:date_published => from..to ).each do |s|
       s.landings.each do |l|
         catches.concat l.catches
       end
