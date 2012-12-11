bd39307e95d8be5e7fa8489306b71c210fb4288c
ahharvey <ah.harvey@gmail.com>
2012-12-11 14:08:39 +0700

    changes file_uploader to file

--- lib/import_excel_data.rb
+++ lib/import_excel_data.rb
@@ -14,76 +14,76 @@ class ImportExcelData
     # form A
     
     if xls.sheets.include?('Form A - Fleet')
-      xls.default_sheet = 'Form A - Fleet'
+      # xls.default_sheet = 'Form A - Fleet'
     
     
-      for d in 2..xls.last_row
-        reg = xls.cell(d,"B").to_s
-        name = xls.cell(d,"C")
-        type = xls.cell(d,"D")
-        engine = xls.cell(d,"E").to_s
-        power = xls.cell(d,"F").to_s
-        fishing_area = xls.cell(d,"G").to_s
-        dep_time = Time.now+xls.cell(d,"H") rescue Time.now
-        arr_time = Time.now+xls.cell(d,"I") rescue Time.now
-        sail = xls.cell(d,"J")
-        fuel = xls.cell(d,"K").to_s
-        crew = xls.cell(d,"L").to_s
-        gear = xls.cell(d,"M").downcase rescue ''
-        weight = xls.cell(d,"N").to_i
-        qty = xls.cell(d,"O").to_i
-        value = xls.cell(d,"P").to_i
+      # for d in 2..xls.last_row
+      #   reg = xls.cell(d,"B").to_s
+      #   name = xls.cell(d,"C")
+      #   type = xls.cell(d,"D")
+      #   engine = xls.cell(d,"E").to_s
+      #   power = xls.cell(d,"F").to_s
+      #   fishing_area = xls.cell(d,"G").to_s
+      #   dep_time = Time.now+xls.cell(d,"H") rescue Time.now
+      #   arr_time = Time.now+xls.cell(d,"I") rescue Time.now
+      #   sail = xls.cell(d,"J")
+      #   fuel = xls.cell(d,"K").to_s
+      #   crew = xls.cell(d,"L").to_s
+      #   gear = xls.cell(d,"M").downcase rescue ''
+      #   weight = xls.cell(d,"N").to_i
+      #   qty = xls.cell(d,"O").to_i
+      #   value = xls.cell(d,"P").to_i
 
-        unless reg.blank?
-          gear_id = Gear.where("LOWER(code) = ?", gear).first.id rescue 0
+      #   unless reg.blank?
+      #     gear_id = Gear.where("LOWER(code) = ?", gear).first.id rescue 0
 
-          landing = Landing.new(power: power, fishing_area: fishing_area, type: type, vessel_ref: reg, vessel_name: name, engine: engine, sail: sail, fuel: fuel, crew: crew, weight: weight, quantity: qty, value: value, time_in: arr_time, time_out: dep_time, gear_id: gear_id.to_i)
-          landing.save
-          logger.info(landing.errors.full_messages)
-        end
-      end
+      #     landing = Landing.new(power: power, fishing_area: fishing_area, type: type, vessel_ref: reg, vessel_name: name, engine: engine, sail: sail, fuel: fuel, crew: crew, weight: weight, quantity: qty, value: value, time_in: arr_time, time_out: dep_time, gear_id: gear_id.to_i)
+      #     landing.save
+      #     logger.info(landing.errors.full_messages)
+      #   end
+      # end
     end
     
     # form B
     if xls.sheets.include?('Form B - Catch')
-      xls.default_sheet = 'Form B - Catch'
+      # xls.default_sheet = 'Form B - Catch'
     
     
-      for i in 2..xls.last_row
-        species = xls.cell(i,"B").downcase  rescue ''
-        length = xls.cell(i,"C")
-        weight = xls.cell(i,"D")
-        unless species.blank?
-          fish_id = Fish.where("LOWER(code) = ?", species).first.id rescue 0
+      # for i in 2..xls.last_row
+      #   species = xls.cell(i,"B").downcase  rescue ''
+      #   length = xls.cell(i,"C")
+      #   weight = xls.cell(i,"D")
+      #   unless species.blank?
+      #     fish_id = Fish.where("LOWER(code) = ?", species).first.id rescue 0
         
-          catch = Catch.new(fish_id: fish_id.to_i, length: length, weight: weight)
-          catch.save
-        end
-      end
+      #     catch = Catch.new(fish_id: fish_id.to_i, length: length, weight: weight)
+      #     catch.save
+      #   end
+      # end
     end
     
     # Survey
     if xls.sheets.include?('Survey')
-      xls.default_sheet = 'Survey'
+    #   xls.default_sheet = 'Survey'
     
-      fishery = xls.cell(1,"B").downcase  rescue ''
-      kabupaten = xls.cell(2,"B")
-      code_desa = xls.cell(3,"B").downcase  rescue ''
-      date = xls.cell(4,"B").to_time
-      start_time = date+xls.cell(5,"B")
-      end_time = date+xls.cell(6,"B")
-      fleet_observer = xls.cell(7,"B")
-      catch_scribe = xls.cell(8,"B")
-      catch_measure = xls.cell(9,"B")
+    #   fishery = xls.cell(1,"B").downcase  rescue ''
+    #   kabupaten = xls.cell(2,"B")
+    #   code_desa = xls.cell(3,"B").downcase  rescue ''
+    #   date = xls.cell(4,"B").to_time
+    #   start_time = date+xls.cell(5,"B")
+    #   end_time = date+xls.cell(6,"B")
+    #   fleet_observer = xls.cell(7,"B")
+    #   catch_scribe = xls.cell(8,"B")
+    #   catch_measure = xls.cell(9,"B")
         
-      unless fishery.blank?
-        desa_id = Desa.where("LOWER(code) = ?", code_desa).first.id rescue 0
-        fishery_id = Fishery.where("LOWER(code) = ?", fishery).first.id rescue 0
+    #   unless fishery.blank?
+    #     desa_id = Desa.where("LOWER(code) = ?", code_desa).first.id rescue 0
+    #     fishery_id = Fishery.where("LOWER(code) = ?", fishery).first.id rescue 0
 
-        survey = Survey.new(fishery_id: fishery_id.to_i, desa_id: desa_id.to_i, date_published: date, start_time: start_time, end_time: end_time, fleet_observer: fleet_observer, catch_scribe: catch_scribe, catch_measure: catch_measure, user_id: user_id.to_i)
-        survey.save
-      end
-    end
+    #     survey = Survey.new(fishery_id: fishery_id.to_i, desa_id: desa_id.to_i, date_published: date, start_time: start_time, end_time: end_time, fleet_observer: fleet_observer, catch_scribe: catch_scribe, catch_measure: catch_measure, user_id: user_id.to_i)
+    #     survey.save
+    #   end
+    # end
     
   end
 end
