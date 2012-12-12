209a7cff7ca9ba6b19c458b059b273ae62c385c8
ahharvey <ah.harvey@gmail.com>
2012-12-12 23:16:32 +0700

    adds credentials to config vars for non-dev environments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,21 @@
-AWS_CONFIG = YAML.load_file(Rails.root.join("config/credentials.yml"))[Rails.env]['aws']
+if Rails.env.development?
+	AWS_CONFIG = YAML.load_file(Rails.root.join("config/credentials.yml"))[Rails.env]['aws']
+	AWS_KEY = AWS_CONFIG['aws_access_key_id']
+	AWS_SECRET = AWS_CONFIG['aws_secret_access_key']
+	AWS_DIRECTORY = AWS_CONFIG['directory']
+else
+	AWS_KEY = ENV['S3_KEY']
+	AWS_SECRET = ENV['S3_SECRET']
+	AWS_DIRECTORY = ENV['S3_directory']
+end
+
 
 CarrierWave.configure do |config|
-  config.fog_directory  = AWS_CONFIG['directory']
+  config.fog_directory  = AWS_DIRECTORY
   config.storage = :fog
   config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => AWS_CONFIG['aws_access_key_id'],
-      :aws_secret_access_key => AWS_CONFIG['aws_secret_access_key']
+      :aws_access_key_id => AWS_KEY,
+      :aws_secret_access_key => AWS_SECRET
   }
 end
