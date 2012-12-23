800cd8227415d83da503a172f2446d969b7b72fd
ahharvey <ah.harvey@gmail.com>
2012-12-23 09:45:30 +0000

    disables broken migration

--- db/migrate/20121221213000_add_survey_count_to_admin.rb
+++ db/migrate/20121221213000_add_survey_count_to_admin.rb
@@ -1,32 +1,32 @@
 class AddSurveyCountToAdmin < ActiveRecord::Migration
   
-  def self.up
-    add_column :admins, :surveys_count, :integer, :default => 0
-    
-    Admin.reset_column_information
-  	Admin.all.each do |a|
-  		Admin.reset_counters a.id, :surveys
-  	end
+#  def self.up
+#    add_column :admins, :surveys_count, :integer, :default => 0
+#    
+#    Admin.reset_column_information
+#    Admin.find_each do |a|
+#      Admin.reset_counters a.id, :surveys
+#    end#
 
-    add_column :fisheries, :surveys_count, :integer, :default => 0
-    
-    Fishery.reset_column_information
-  	Fishery.all.each do |f|
-  		Fishery.reset_counters f.id, :surveys
-  	end 	
+#    add_column :fisheries, :surveys_count, :integer, :default => 0
+#    
+#    Fishery.reset_column_information
+#    Fishery.find_each do |f|
+#      Fishery.reset_counters f.id, :surveys
+#    end	#
 
-    add_column :desas, :surveys_count, :integer, :default => 0
-    
-    Desa.reset_column_information
-  	Desa.all.each do |d|
-  		Desa.reset_counters d.id, :prereps
-  	end
-  end
+#    add_column :desas, :surveys_count, :integer, :default => 0
+#    
+#    Desa.reset_column_information
+#    Desa.find_each do |d|
+#      Desa.reset_counters d.id, :surveys
+#    end
+#  end#
 
-  def self.down
-    remove_column :admins, :surveys_count
-    remove_column :fisheries, :surveys_count
-    remove_column :desas, :surveys_count
-  end
+#  def self.down
+#    remove_column :admins, :surveys_count
+#    remove_column :fisheries, :surveys_count
+#    remove_column :desas, :surveys_count
+#  end
 
 end
