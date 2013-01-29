993eeb23fb6c88a2db24c146121f6ebc30cecb96
Richard Sylvester <richard@richard-Lenovo-G570.(none)>
2013-01-29 10:38:25 +0100

    Added logbook uniqueness validation.

--- app/models/logbook.rb
+++ app/models/logbook.rb
@@ -1,10 +1,9 @@
 class Logbook < ActiveRecord::Base
-  
   belongs_to :user
   belongs_to :admin
   belongs_to :fishery
   has_many :logged_days
   attr_accessible :date, :user_id, :admin_id, :fishery_id
 
-  
+  validates_uniqueness_of :date, :scope => :fishery_id
 end
