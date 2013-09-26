7916591fb464ad8d0dfe1872b20ca7b93690c62b
AHH <ah.harvey@gmail.com>
2013-09-26 18:25:58 +0700

    debugs

--- app/controllers/excel_files_controller.rb
+++ app/controllers/excel_files_controller.rb
@@ -6,7 +6,7 @@ class ExcelFilesController < InheritedResources::Base
   def show
     require 'roo'
     @excel_file = ExcelFile.find(params[:id])
-    @file = File.join(RAILS.root, @excel_file.file.url.to_s)
+    @file = File.join(Rails.root, @excel_file.file.url.to_s)
     @rspreadsheet = Excelx.new(@file) #Excelx.new("http://i-fish.dev/uploads/excel_file/file/7/Database.template__3_.xlsx")#
   end
 end
