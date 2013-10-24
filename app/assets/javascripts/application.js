74d126b4d0f40c75f77e85e0623640a150d2c3f6
AHH <ah.harvey@gmail.com>
2013-10-24 21:18:21 +0700

    udpates jquery files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery-ui.all
 //= require jquery.ui.datepicker
 //= require bootstrap
 //= require bootstrap-switch
