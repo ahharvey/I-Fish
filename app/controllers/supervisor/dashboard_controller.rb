dd66c3795b9b5652f344bcfc1613d4a248b47056
AHH <ah.harvey@gmail.com>
2014-01-18 22:59:02 +0700

    [#63971020 #63971040 #63971008 #63971010] adds historical surveys and logbooks summary. adds approved/ pending indicators to logbook and survey index. adds fisher names and locations to logbook dashboard

--- app/controllers/supervisor/dashboard_controller.rb
+++ app/controllers/supervisor/dashboard_controller.rb
@@ -2,8 +2,27 @@ class Supervisor::DashboardController < ApplicationController
 	#authorize_resource :class => false
   def index
     authorize! :index, 'supervisor/dashboard'
-		@surveys = Survey.where(approved: false)
-		@logbooks = Logbook.where(approved: false)
+		
+    @surveys = Survey.scoped
+    @unapproved_surveys = @surveys.where(approved: false)
+    @historical_surveys = @surveys.
+      where(approved: true).
+      where('date_published > ?', Date.today.beginning_of_month-1.year).
+      group_by { |t| t.date_published.beginning_of_month }
+		
+    @logbooks = Logbook.scoped
+    @unapproved_logbooks = @logbooks.where(approved: false)
+    @historical_logbooks = @logbooks.
+      where(approved: true).
+      where('date > ?', Date.today.beginning_of_month-1.year).
+      group_by { |t| t.date.beginning_of_month }
+
 		@admins = Admin.where(approved: false)
+
+    @historical_records = []
+
+    (@historical_surveys.keys + @historical_logbooks.keys).uniq.sort.each { |month|
+      @historical_records << { month: month, survey: @historical_surveys[month].try(:length) || 0, logbook: @historical_logbooks[month].try(:length) || 0 }
+    }
 	end
 end
