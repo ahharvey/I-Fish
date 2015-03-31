45f1c5e5b1cb42f64e47251d371e4cf4c28566f4
AHH <ah.harvey@gmail.com>
2015-04-01 00:44:17 +1000

    adds size limits to nav avatar

--- app/presenters/admin_presenter.rb
+++ app/presenters/admin_presenter.rb
@@ -3,7 +3,7 @@ class AdminPresenter < BasePresenter
 
   def avatar
     if admin.avatar?
-      image_tag admin.avatar_url(:thumb), title: admin.name, class: 'img-circle'
+      image_tag admin.avatar_url(:thumb), title: admin.name, class: 'img-circle', style: 'width:30px;height:30px;'
     else
       content_tag :div,  class: 'img-circle avatar', style: 'width:30px;height:30px;' do
         "#{admin.firstname[0].capitalize}#{admin.lastname[0].capitalize}"
