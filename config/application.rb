12fedc6f8cdedafb9fc62a5b909b37dcd42ca8ea
AHH <ah.harvey@gmail.com>
2015-05-10 19:50:09 +0700

    updates sidekiq config

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module IFish
 
     config.active_record.raise_in_transactional_callbacks = true
 
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
