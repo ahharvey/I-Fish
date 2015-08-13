7d9b1d138a2c53e7f97470fa363a76e5fbc54d9c
AHH <ah.harvey@gmail.com>
2015-08-14 02:00:05 +0700

    improves detection of whodunnit type

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,15 +1,15 @@
 # config/initializers/paper_trail.rb
 PaperTrail::Rails::Engine.eager_load!
 module PaperTrail
-class Version < ActiveRecord::Base
-  def whodunnit
-    return nil 							if self[:whodunnit].blank? 
-    return self[:whodunnit] if self[:whodunnit].is_a? Integer
-    return self[:whodunnit] if self[:whodunnit] == 'Guest'
+	class Version < ActiveRecord::Base
+	  def whodunnit
+	    return nil 										if self[:whodunnit].blank? 
+	    return self[:whodunnit].to_i 	if Integer( self[:whodunnit] ).is_a? Integer rescue false
+	    return self[:whodunnit] 			if self[:whodunnit] == 'Guest'
 
-    target_klass, target_id = self[:whodunnit].split(':')
-    eval(target_klass).find(target_id)
-  end
+	    target_klass, target_id = self[:whodunnit].split(':')
+	    eval(target_klass).find(target_id)
+	  end
 
-end
+	end
 end
